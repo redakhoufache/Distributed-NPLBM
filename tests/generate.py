f93271b93287473a468e1c59520b9a8d61723a2b
MedAnisse <med-anisse.belhadj@outlook.com>
2023-08-10 21:02:58 +0200

    multidimensional test generation

--- tests/generate.py
+++ tests/generate.py
@@ -8,7 +8,8 @@ import sys
 observations=int(sys.argv[1])
 features=int(sys.argv[2])
 cluster=(int(sys.argv[3]),int(sys.argv[4]))
-X, y ,modes= make_blobs(n_samples=25000, centers=cluster[0]*cluster[1], n_features=1,random_state=0,return_centers= True)
+dim_input=int(sys.argv[5])
+X, y ,modes= make_blobs(n_samples=25000, centers=cluster[0]*cluster[1], n_features=dim_input,random_state=0,return_centers= True)
 print("centers=",len(modes))
 print(type(modes))
 data_shape=(observations,features)
@@ -24,38 +25,71 @@ while(True):
             break
 print(row_sizes)
 print(col_sizes)
-d = [1, 1]
-A = np.diag(d)
-dim=modes[0].size
-data=list()
-membersip=list()
-for i in range(cluster[0]):
-    row_block=list()
-    row_block_membersip=list()
-    for j in range(cluster[1]):
-        block_size=col_sizes[j]*row_sizes[i]
-        rv = multivariate_normal.rvs(mean=modes[i*cluster[1]+j], cov=(0.5+(2.0-0.5)*np.random.random()),size=block_size)
-        x = np.arange(block_size, dtype=int)
-        block_membersip=np.full_like(x, (i*cluster[1]+j), dtype=int,shape=(row_sizes[i],col_sizes[j]))
-        rv=rv.reshape(row_sizes[i],col_sizes[j],dim)
-        row_block.append(rv)
-        row_block_membersip.append(block_membersip)
-    data.append(np.concatenate(row_block, axis=1))
-    membersip.append(np.concatenate(row_block_membersip, axis=1))
-data=np.concatenate(data, axis=0)
-membersip=np.concatenate(membersip, axis=0)
-print(data.shape)
-print(membersip.shape)
-with open("data/synthetic_"+str(data.shape[0])+"_"+str(data.shape[1])+"_"+str(cluster[0]*cluster[1])+".csv", 'w') as f:
-    f.write(','.join([str(x) for x in range(data.shape[1])]))
-    f.write("\n")
-    for i in range(data.shape[0]):
-        line_str=list()
-        for j in range(data.shape[1]):
-            line_str.append(':'.join([str(x) for x in data[i][j].tolist()]))
-        line=(','.join([y for y in line_str]))
-        f.write(line)
+if dim_input==1:
+    dim=modes[0].size
+    data=list()
+    membersip=list()
+    for i in range(cluster[0]):
+        row_block=list()
+        row_block_membersip=list()
+        for j in range(cluster[1]):
+            block_size=col_sizes[j]*row_sizes[i]
+            rv = multivariate_normal.rvs(mean=modes[i*cluster[1]+j], cov=(0.5+(2.0-0.5)*np.random.random()),size=block_size)
+            x = np.arange(block_size, dtype=int)
+            block_membersip=np.full_like(x, (i*cluster[1]+j), dtype=int,shape=(row_sizes[i],col_sizes[j]))
+            rv=rv.reshape(row_sizes[i],col_sizes[j],dim)
+            row_block.append(rv)
+            row_block_membersip.append(block_membersip)
+        data.append(np.concatenate(row_block, axis=1))
+        membersip.append(np.concatenate(row_block_membersip, axis=1))
+    data=np.concatenate(data, axis=0)
+    membersip=np.concatenate(membersip, axis=0)
+    print(data.shape)
+    print(membersip.shape)
+    with open("data/synthetic_"+str(data.shape[0])+"_"+str(data.shape[1])+"_"+str(cluster[0]*cluster[1])+".csv", 'w') as f:
+        f.write(','.join([str(x) for x in range(data.shape[1])]))
         f.write("\n")
+        for i in range(data.shape[0]):
+            line_str=list()
+            for j in range(data.shape[1]):
+                line_str.append(':'.join([str(x) for x in data[i][j].tolist()]))
+            line=(','.join([y for y in line_str]))
+            f.write(line)
+            f.write("\n")
+else:
+    d = [1]*dim_input
+    A = np.diag(d)
+    dim=modes[0].size
+    data=list()
+    membersip=list()
+    for i in range(cluster[0]):
+        row_block=list()
+        row_block_membersip=list()
+        for j in range(cluster[1]):
+            block_size=col_sizes[j]*row_sizes[i]
+            rv = multivariate_normal.rvs(mean=modes[i*cluster[1]+j], cov=A,size=block_size)
+            x = np.arange(block_size, dtype=int)
+            block_membersip=np.full_like(x, (i*cluster[1]+j), dtype=int,shape=(row_sizes[i],col_sizes[j]))
+            rv=rv.reshape(row_sizes[i],col_sizes[j],dim)
+            row_block.append(rv)
+            row_block_membersip.append(block_membersip)
+        data.append(np.concatenate(row_block, axis=1))
+        membersip.append(np.concatenate(row_block_membersip, axis=1))
+    data=np.concatenate(data, axis=0)
+    membersip=np.concatenate(membersip, axis=0)
+    print(data.shape)
+    print(membersip.shape)
+    with open("data/synthetic_"+str(data.shape[0])+"_"+str(data.shape[1])+"_"+str(cluster[0]*cluster[1])+".csv", 'w') as f:
+        f.write(','.join([str(x) for x in range(data.shape[1])]))
+        f.write("\n")
+        for i in range(data.shape[0]):
+            line_str=list()
+            for j in range(data.shape[1]):
+                line_str.append(':'.join([str(x) for x in data[i][j].tolist()]))
+            line=(','.join([y for y in line_str]))
+            f.write(line)
+            f.write("\n")
+
 datasets=""
 col_size_str='/'.join([str(y) for y in col_sizes])
 row_size_str='/'.join([str(y) for y in row_sizes])
