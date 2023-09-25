70c514c77eaf9794b69b3927e851c4907467c845
MedAnisse <med-anisse.belhadj@outlook.com>
2023-09-25 16:55:06 +0200

    generate data from 20K to 100K

--- src/main/scala/DAVID/Common/DataGeneration.scala
+++ src/main/scala/DAVID/Common/DataGeneration.scala
@@ -5,6 +5,7 @@ import DAVID.Common.Tools.allEqual
 import breeze.linalg.{DenseMatrix, DenseVector, diag}
 import breeze.stats.distributions.{MultivariateGaussian, RandBasis}
 
+import java.io.{FileOutputStream, PrintStream}
 import scala.util.Random
 
 object DataGeneration  {
@@ -90,10 +91,10 @@ object DataGeneration  {
 
     require(modeLengths.forall(_ == modeLengths.head), "In LBM case, every column must have the same number of modes")
     require(covLengths.forall(_ == covLengths.head), "In LBM case, every column must have the same number of covariances matrices")
-    require(K == covLengths.head, "modes and covariances K do not match")
+    require(K == covLengths.head, s"modes and covariances K do not match ${K} == ${covLengths.head}")
     require(modes.length == covariances.length, "modes and covariances L do not match")
-    require(sizeClusterRow.length == K)
-    require(sizeClusterCol.length == L)
+    require(sizeClusterRow.length == K,s"${sizeClusterRow.length}->$K")
+    require(sizeClusterCol.length == L,s"${sizeClusterCol.length}->$L")
 
     val sizeClusterRowEachColumn = List.fill(L)(sizeClusterRow)
     val dataPerBlock: List[List[DenseMatrix[DenseVector[Double]]]] = generateDataPerBlock(
@@ -233,6 +234,72 @@ object DataGeneration  {
       DenseMatrix.tabulate[DenseVector[Double]](data.rows,data.cols){ (i,j) => data(newRowIndex(i), newColIndex(j))}
     } else data
   }
+  def generate_20k_10k_10K_3L(path:String): Unit = {
+    val sizeClusterRows = List(
+      List(2045, 2016, 1959, 2049, 1973, 2009, 2026, 1955, 1984, 1984),
+      List(4032, 3969, 3987, 3988, 3873, 4056, 4059, 4048, 4111, 3877),
+      List(6014, 6084, 6075, 5881, 6092, 5903, 5917, 5933, 6041, 6060),
+      List(8036, 7960, 8111, 8026, 7943, 8004, 7967, 7979, 8020, 7954),
+      List(10054, 9959, 10009, 9906, 10093, 9996, 9848, 10114, 10082, 9939)
+    )
+    val sizeClusterCols = List(
+      List(16, 25, 19),
+      List(19, 20, 21),
+      List(20, 25, 15),
+      List(18, 20, 22),
+      List(15, 25, 20)
+    )
+    val datasetNames = List(
+      "synthetic_20000_60_30.csv",
+      "synthetic_40000_60_30.csv",
+      "synthetic_60000_60_30.csv",
+      "synthetic_80000_60_30.csv",
+      "synthetic_100000_60_30.csv"
+    )
+    val modes10k3L = List(
+      List(DenseVector(0.97627008), DenseVector(4.30378733), DenseVector(2.05526752),
+        DenseVector(0.89766366), DenseVector(-1.52690401), DenseVector(2.91788226),
+        DenseVector(-1.24825577), DenseVector(7.83546002), DenseVector(9.27325521), DenseVector(8.89337834)),
+      List(DenseVector(-2.33116962), DenseVector(5.83450076), DenseVector(0.5778984),
+        DenseVector(1.36089122), DenseVector(8.51193277), DenseVector(-8.57927884),
+        DenseVector(-8.25741401), DenseVector(-9.59563205), DenseVector(6.65239691), DenseVector(0.43696644)),
+      List(DenseVector(5.56313502), DenseVector(7.40024296), DenseVector(9.57236684),
+        DenseVector(5.98317128), DenseVector(-0.77041275), DenseVector(5.61058353),
+        DenseVector(-7.63451148), DenseVector(2.79842043), DenseVector(-7.13293425), DenseVector(-1.7067612)))
+    val covariances10K3L = List(List(DenseMatrix(0.669336507796175), DenseMatrix(1.7612065703879582),
+      DenseMatrix(1.3862592599171373), DenseMatrix(1.1238622175611195), DenseMatrix(1.2457670824447593),
+      DenseMatrix(1.6931717416325167), DenseMatrix(1.5972692066111005), DenseMatrix(1.1159170518255292),
+      DenseMatrix(1.219057950452216), DenseMatrix(0.8289552408204514)),
+      List(DenseMatrix(1.8518144848803162), DenseMatrix(1.079276132540391),
+        DenseMatrix(1.9446262792984377), DenseMatrix(0.8714880183032959),
+        DenseMatrix(1.532140243991034), DenseMatrix(1.084828014988245),
+        DenseMatrix(1.2964549237719827), DenseMatrix(0.5460550074747246),
+        DenseMatrix(1.8643446504119405), DenseMatrix(1.876902873474071)),
+      List(DenseMatrix(0.9333442461964505), DenseMatrix(1.9731924700985997),
+        DenseMatrix(0.7524495254489219), DenseMatrix(0.9777400894782611),
+        DenseMatrix(1.7316683965907), DenseMatrix(1.0062588416920026),
+        DenseMatrix(1.9621619190951707), DenseMatrix(1.9494185295668427),
+        DenseMatrix(1.8494073742054244), DenseMatrix(0.5954647897783252)))
+    (0 until 5).map(index_dataset=> {
+      val data_generated = randomLBMDataGeneration(modes = modes10k3L,
+        covariances = covariances10K3L,
+        sizeClusterRow = sizeClusterRows(index_dataset),
+        sizeClusterCol = sizeClusterCols(index_dataset))
+      /*val nLaunches = 10*/
+      val data10K3L = (0 until 20000 + (index_dataset * 20000)).par.map(i => {
+        (0 until 60).par.map(j => {
+          data_generated.valueAt(i, j)
+        }).toList
+      }).toList
+      System.setOut(new PrintStream(
+        new FileOutputStream(s"$path/data/${datasetNames(index_dataset)}"))
+      )
+      System.out.println((0 until 60).map(_.toString).mkString(","))
+      data10K3L.map(line=>
+        System.out.println(line.map(cell=>cell.valueAt(0).toString).mkString(","))
+      )
+    })
 
+  }
 
 }
