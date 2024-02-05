15d89dd85d5121363c8ee1205d5fd7275636f1cf
khoufache <khoufache@lipn.univ-paris13.fr>
2024-02-05 01:46:18 +0100

    refactoring code

--- tests/generate_data_run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash 
-
-pip3 install -U scikit-learn
-pip3 install scipy
-pip3 install numpy
-
-python3 generate.py 20000 20 10 4 1
--- tests/make_jar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PATH="$PATH:/opt/bitnami/spark/apache-maven-3.9.1/bin"
-cd Distributed-NPLBM
-mvn package -DskipTests 
